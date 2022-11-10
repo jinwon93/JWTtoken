5cb97708a2b9c33da825481f07c25f0bb5db08aa
jinwon93 <kjw8208@nate.com>
2022-11-10 19:14:50 +0900

    swaggerApi

--- src/main/java/com/token/domains/users/presentation/UserController.java
+++ src/main/java/com/token/domains/users/presentation/UserController.java
@@ -5,21 +5,25 @@ import com.token.domains.users.application.UserService;
 import com.token.domains.users.application.dto.TokenResponse;
 import com.token.domains.users.application.dto.UserRequest;
 import com.token.domains.users.domain.UsersEntity;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.h2.engine.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequiredArgsConstructor
+
+@RequestMapping("/")
 @RestController
+@Api(tags = {"JoinMember"})
+@RequiredArgsConstructor
 public class UserController {
 
   private final UserService userService;
 
+  @ApiOperation(value = "회원가입" , response = UsersEntity.class)
   @PostMapping("/user/signUp")
   public ResponseEntity signUp(@RequestBody UserRequest userRequest) {
     return userService.findByUserId(userRequest.getUserId()).isPresent()
@@ -27,12 +31,15 @@ public class UserController {
         : ResponseEntity.ok(userService.signUp(userRequest));
   }
 
+
+  @ApiOperation(value = "로그인" , response = UsersEntity.class)
   @PostMapping("/user/signIn")
   public ResponseEntity<TokenResponse> signIn(@RequestBody UserRequest userRequest) throws Exception {
 
     return ResponseEntity.ok().body(userService.signIn(userRequest));
   }
 
+  @ApiOperation(value = "회원정보" , response = UsersEntity.class)
   @GetMapping("/info")
   public ResponseEntity<List<UsersEntity>> findUser() {
     return ResponseEntity.ok().body(userService.findUsers());
